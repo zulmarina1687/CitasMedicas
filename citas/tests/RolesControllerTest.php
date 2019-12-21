9ff5412b20d3aa4893101f105ef41daa38f8ff3e
Esejje <38010954+esejjec@users.noreply.github.com>
2019-12-20 23:37:35 -0500

    Update RolesControllerTest.php

--- citas/tests/RolesControllerTest.php
+++ citas/tests/RolesControllerTest.php
@@ -54,5 +54,17 @@ class RolesController extends TestCase{
          $this->assertEquals('nene',$array['0']);
         
      }*/
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame(1, count($stack));
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
+    }
 
 }
